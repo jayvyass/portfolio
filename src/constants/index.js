a3c1cbe9a4d75e8364f15390982d7a81fe826b8e
jayvyass <jayv.iflair@gmail.com>
2024-07-04 15:46:34 +0530

    first commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -99,7 +99,7 @@ const experiences = [
     iconBg: "#E6DEDD",
     date: "Feb 2024 - Present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Developing and maintaining web applications using PHP and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
